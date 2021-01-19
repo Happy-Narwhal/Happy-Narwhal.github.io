509468176a870db7b455ba55d9892b83795d0b7b
Bjorn Linder <bjornishungry@hotmail.com>
2021-01-18 18:26:02 -0600

    setup application to run on sinatra with erb templating

---
+++ server.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'rest-client'
+
+set :views, File.dirname(__FILE__) + '/views'
+# set :public_folder, File.dirname(__FILE__) + '/public'
+
+get '/' do
+  erb :home
+end
+
+get '/:path' do
+  @path = params[:path].chomp('.html')
+  begin
+    erb @path.to_sym, layout: :layout
+  rescue Errno::ENOENT
+    erb :home
+  end
+end
