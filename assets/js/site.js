d95b52cea54efeb458409521c0c19bd63b58a9d1
Bjorn Linder <bjorn@eventmy.com>
2024-01-21 16:04:19 +0800

    touching up text, fixed graphic link

--- assets/js/site.js
+++ assets/js/site.js
@@ -12,16 +12,13 @@ $(document).ready(function () {
   });
 
   $(".bar1").click(function () {
-    $(".meet").prop("checked", true);
+    $("#rails").prop("checked", true);
   })
   $(".bar2").click(function () {
-    $(".plan").prop("checked", true);
+    $("#mern").prop("checked", true);
   })
   $(".bar3").click(function () {
-    $(".develop").prop("checked", true);
-  })
-  $(".bar4").click(function () {
-    $(".launch").prop("checked", true);
+    $("#jekyll").prop("checked", true);
   })
 
   $(".bar").click(function () {
