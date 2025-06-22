2d969797982b476bf4dcd645a253dc6c833831df
Bjorn Linder <bjorn@eventmy.com>
2025-06-21 20:47:21 -0500

    trying to get contact form working.

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,16 +21,10 @@
       check = false;
     }
 
-    if ($(website).val().trim() == '') {
-      showValidate(website);
-      check = false;
-    }
-
     if ($(phone).val().trim() == '') {
       showValidate(phone);
       check = false;
     }
-
 
     if ($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
       showValidate(email);
@@ -43,15 +37,45 @@
     }
 
     e.preventDefault();
+//      url: "https://script.google.com/macros/s/AKfycbxwLBatp-RP7v2UWSffxSCCqrpIZLnol4uQa6qYuyHfoGs3J1EAr2UkyLFpcw_HJYs37w/exec",
 
     if (!check)
       return false;
 
+    // const data = new FormData(e.currentTarget);
+    // var data = $(".contact1-form").serialize();
+    // redirect: "follow",
+    // "Access-Control-Allow-Origin": '*',//'http://script.google.com',
+    // mode: "no-cors",
+
+    // contentType: "application/javascript",
+
+    var data = {email: "email@address.com"};
+
     $.ajax({
-      url: "https://script.google.com/macros/s/AKfycbxwLBatp-RP7v2UWSffxSCCqrpIZLnol4uQa6qYuyHfoGs3J1EAr2UkyLFpcw_HJYs37w/exec",
+      url: "https://script.google.com/macros/s/AKfycbxOPQes5sMsioPIfW4VDeiTydxNz26sQTb2K_5M7RHy1wUJ80gCG6pp3vERFC6iGFpb9Q/exec",
       method: "POST",
-      dataType: "json",
-      data: $(".contact1-form").serialize(),
+      crossDomain: true,
+      async: true,
+      // dataType: "jsonp",
+      // jsonpCallback: 'callback',
+      data: data,
+      responseType:'application/json',
+      withCredentials: false,
+      // xhrFields: {
+      //     withCredentials: false
+      // },
+      // "Access-Control-Allow-Origin": 'script.google.com',
+      contentType: 'application/json',
+      "X-Content-Type-Options": "nosniff",
+      headers: {
+        // "Content-Type": "text/plain;charset=utf-8",
+        // "Content-Type": "application/json",
+        // "X-Content-Type-Options": "nosniff",
+        "Access-Control-Allow-Origin": 'script.google.com',
+        // "Content-Length": data.length,
+        // "Host": "script.google.com"
+      },
       success: function (response) {
         if (response.result == "success") {
           $('.contact1-form')[0].reset();
@@ -59,11 +83,11 @@
           return true;
         }
         else {
-          alert("Something went awry. This must be a sign of the end.")
+          alert("Something went awry response. This must be a sign of the end.")
         }
       },
       error: function (response) {
-        alert("Something went awry. This must be a sign of the end.")
+        alert("Something went awry ajax error. This must be a sign of the end.")
       }
     })
   });
